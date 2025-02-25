06f3183fa583ccb2dd426403dfa529dcb4a913af
Matthew Eldridge <matthew_eldridge@wordbank.com>
2025-02-25 13:13:58 -0700

    Add All Pages

---
+++ pages/pagetest.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { data: home } = await useAsyncData(() =>
+  queryCollection("content").path("/mypage").first()
+);
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description,
+});
+</script>
+
+<template>
+  <div class="container mx-auto p-10 bg-gray-100">
+    <h1 class="text-4xl font-bold mb-8">
+      {{ home?.title }}
+    </h1>
+    <div class="prose">
+      <ContentRenderer v-if="home" :value="home" />
+      <div v-else>Home not found</div>
+    </div>
+  </div>
+</template>
