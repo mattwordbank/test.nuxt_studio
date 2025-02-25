edf724774a7a778b386f1620a111c494152660eb
mattwordbank <75269338+mattwordbank@users.noreply.github.com>
2025-02-25 20:26:02 +0000

    Deploying to gh-pages from @ mattwordbank/test.nuxt_studio@2d73deb83306f7e240fffdd7943704a74c6f8ad0 🚀

---
+++ _nuxt/BwJax75M.js
@@ -0,0 +1,1 @@
+import{l as r,f as t,g as o,P as s}from"./CA5f3yfQ.js";const a={};function n(e,c){return o(),t("thead",null,[s(e.$slots,"default")])}const f=r(a,[["render",n]]);export{f as default};
