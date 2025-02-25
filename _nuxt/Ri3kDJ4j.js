7336f3e62b919856f11e5613607f1740ea6c986d
mattwordbank <75269338+mattwordbank@users.noreply.github.com>
2025-02-25 20:01:35 +0000

    Deploying to gh-pages from @ mattwordbank/test.nuxt_studio@c18e20bc43386fc7099fe239fc69c03fbdb7e9d8 🚀

---
+++ _nuxt/Ri3kDJ4j.js
@@ -0,0 +1,1 @@
+import{l as o,f as r,g as s,N as t}from"./BTfZTVj9.js";const l={};function n(e,c){return s(),r("ol",null,[t(e.$slots,"default")])}const f=o(l,[["render",n]]);export{f as default};
--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <header class="container mx-auto p-10 bg-gray-100 mb-4">
-      <h1>My Nuxt App</h1>
-    </header>
-    <slot />
-  </div>
-</template>
