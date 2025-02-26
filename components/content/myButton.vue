ea54df88eaa4b52fe9331ac81b20ac41a6103e99
Matthew Eldridge <matthew_eldridge@wordbank.com>
2025-02-26 09:39:20 -0700

    Update myButton.vue

--- components/content/myButton.vue
+++ components/content/myButton.vue
@@ -1,5 +1,17 @@
 <template>
-  <a href="/" class="bg-blue-500 text-white px-4 py-2 rounded-md inline-block">
+  <a
+    :href="href"
+    class="bg-blue-500 text-white px-4 py-2 rounded-md inline-block"
+  >
     <slot name="button-label" />
   </a>
 </template>
+
+<script setup>
+defineProps({
+  href: {
+    type: String,
+    default: "/",
+  },
+});
+</script>
